d2881c35f5e39db17e10adc7e8efcf4b00fc760e
Matthew Webb <matt.d.webb@icloud.com>
2017-02-17 21:04:33 +0000

    change directory structure and moved to ES6 client side class

---
+++ src/client/app.js
@@ -0,0 +1,44 @@
+"use strict";
+
+import Chat from './service/chat';
+import {validNick} from '../shared/util'
+
+class Client {
+    constructor () {
+        let btn = document.getElementById('startButton'),
+            userNameInput = document.getElementById('userNameInput');
+
+        btn.onclick = () => {
+            this.startChat(userNameInput.value);
+        };
+
+        userNameInput.addEventListener('keypress', (e) => {
+            let key = e.which || e.keyCode;
+
+            if (key === 13) {
+                this.startChat(userNameInput.value);
+            }
+        });
+    }
+
+    startChat(nick) {
+        let nickErrorText = document.querySelector('#startMenu .input-error');
+
+        if (validNick(nick)) {
+            nickErrorText.style.opacity = 0;
+            this.nick = nick;
+        } else {
+            nickErrorText.style.opacity = 1;
+            return false;
+        }
+
+        this.chat = new Chat(this.nick);
+
+        document.getElementById('startMenu').style.display = 'none';
+        document.getElementById('chatbox').style.display = 'block';
+    }
+}
+
+window.onload = () => {
+    new Client();
+};
--- src/index.js
+++ src/index.js
@@ -1,22 +1,63 @@
-// Setup basic express server
+
+'use strict';
+
 import express from 'express';
+import http from 'http';
+import SocketIO from 'socket.io';
+import compression from 'compression';
+import {validNick, findIndex, sanitizeString} from './shared/util';
 
-import Chat from './server/modules/chat';
+let app = express();
+let server = http.Server(app);
+let io = new SocketIO(server);
+let port = process.env.PORT || 3000;
+let users = [];
+let sockets = {};
 
+app.use(compression({}));
+app.use(express['static'](__dirname + '/client'));
 
-const app = express();
-const server = require('http').createServer(app);
+io.on('connection', (socket) => {
+    let nick = socket.handshake.query.nick;
+    let currentUser = {
+        id: socket.id,
+        nick: nick
+    };
 
-const port = process.env.PORT || 3000;
+    if (findIndex(users, currentUser.id) > -1) {
+        console.log('[INFO] User ID is already connected, kicking.');
+        socket.disconnect();
+    } else if (!validNick(currentUser.nick)) {
+        socket.disconnect();
+    } else {
+        console.log('[INFO] User ' + currentUser.nick + ' connected!');
+        sockets[currentUser.id] = socket;
+        users.push(currentUser);
+        io.emit('userJoin', {nick: currentUser.nick});
+        console.log('[INFO] Total users: ' + users.length);
+    }
 
+    socket.on('ding', () => {
+        socket.emit('dong');
+    });
 
-/* eslint-disable no-console */
-server.listen(port, function () {
-  console.log('Server listening at port %d', port);
+    socket.on('disconnect', () => {
+        if (findIndex(users, currentUser.id) > -1) users.splice(findIndex(users, currentUser.id), 1);
+        console.log('[INFO] User ' + currentUser.nick + ' disconnected!');
+        socket.broadcast.emit('userDisconnect', {nick: currentUser.nick});
+    });
+
+    socket.on('userChat', (data) => {
+        let _nick = sanitizeString(data.nick);
+        let _message = sanitizeString(data.message);
+        let date = new Date();
+        let time = ("0" + date.getHours()).slice(-2) + ("0" + date.getMinutes()).slice(-2);
+
+        console.log('[CHAT] [' + time + '] ' + _nick + ': ' + _message);
+        socket.broadcast.emit('serverSendUserChat', {nick: _nick, message: _message});
+    });
 });
 
-// Routing
-app.use(express.static(__dirname + '/public'));
-
-const chat = new Chat({ server: server });
-chat.start();
+server.listen(port, () => {
+    console.log('[INFO] Listening on *:' + port);
+});
